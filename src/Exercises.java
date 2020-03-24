53aaa3757f25849b95de5cb291f08e239ab2eb5f
Reilly <reilly.ford@gmail.com>
2020-03-23 22:05:49 -0400

    4.5 out of 12

--- src/Exercises.java
+++ src/Exercises.java
@@ -67,11 +67,46 @@ public class Exercises {
     }
 
     public int[] bubble(int[] list, boolean ascending) {
-        return null;
+        if (list == null || list.length == 0) {
+			return null;
+		}
+		for (int i = 0; i < list.length - 1; i++) {
+			for (int j = 0; j < list.length - i - 1; j++) {
+				if (list[j] > list[j + 1]) {
+					int temp = list[j];
+					list[j] = list[j + 1];
+					list[j + 1] = temp;
+				}
+			}
+		}
+		if (!ascending) {
+			int i, t;
+			int n = list.length;
+			for (i = 0; i < n / 2; i++) {
+				t = list[i];
+				list[i] = list[n - i - 1];
+				list[n - i - 1] = t;
+			}
+		}
+
+		return list;
     }
 
     public ArrayList<String> bubble(ArrayList<String> list, boolean ascending) {
-        return null;
+        if (list.contains(null) || list == null) {
+			return null;
+		}
+		for (int i = 0; i < list.size() - 1; i++) {
+			for (int j = 0; j < list.size() - 1 - i; j++) {
+				if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+					
+				}
+			}
+		}
+		if (!ascending) {
+			Collections.reverse(list);
+		}
+		return list;
     }
 
     public ArrayList<Integer> insertion(ArrayList<Integer> list, boolean ascending) {
