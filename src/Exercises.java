cae855a9db20f19506907354e1cc3d5e32265422
Reilly <reilly.ford@gmail.com>
2020-03-23 22:06:20 -0400

    5 out of 12

--- src/Exercises.java
+++ src/Exercises.java
@@ -99,7 +99,9 @@ public class Exercises {
 		for (int i = 0; i < list.size() - 1; i++) {
 			for (int j = 0; j < list.size() - 1 - i; j++) {
 				if (list.get(j).compareTo(list.get(j + 1)) > 0) {
-					
+                    String temp = list.get(j);
+					list.set(j, list.get(j + 1));
+					list.set(j + 1, temp);
 				}
 			}
 		}
