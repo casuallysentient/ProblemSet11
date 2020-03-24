9a1c519737f22725696aadeb55e91b7a6993bcf8
Reilly <reilly.ford@gmail.com>
2020-03-23 23:31:54 -0400

    12 out of 12 goodnight mr wilson

--- src/Exercises.java
+++ src/Exercises.java
@@ -325,4 +325,46 @@ public class Exercises {
         String[] combined = combine(merge(firstList, ascending), merge(secondList, ascending), ascending);
         return combined;
     }
+    //12.5
+    public String[] combine(String[] firstList, String[] secondList, boolean ascending) {
+        String[] combination = new String[firstList.length + secondList.length];
+        int firstLocation = 0;
+        int secondLocation = 0;
+        int combineLocation = 0;
+
+        while (firstLocation < firstList.length && secondLocation < secondList.length) {
+            if (ascending) {
+                if (firstList[firstLocation].compareTo(secondList[secondLocation]) < 0) {
+                    combination[combineLocation] = firstList[firstLocation];
+                    firstLocation++;
+                    combineLocation++;
+                } else {
+                    combination[combineLocation] = secondList[secondLocation];
+                    secondLocation++;
+                    combineLocation++;
+                }
+            } else {
+                if (firstList[firstLocation].compareTo(secondList[secondLocation]) > 0) {
+                    combination[combineLocation] = firstList[firstLocation];
+                    firstLocation++;
+                    combineLocation++;
+                } else {
+                    combination[combineLocation] = secondList[secondLocation];
+                    secondLocation++;
+                    combineLocation++;
+                }
+            }
+        }
+        while (firstLocation == firstList.length && secondLocation != secondList.length) {
+            combination[combineLocation] = secondList[secondLocation];
+            secondLocation++;
+            combineLocation++;
+        }
+        while (secondLocation == secondList.length && firstLocation != firstList.length) {
+            combination[combineLocation] = firstList[firstLocation];
+            firstLocation++;
+            combineLocation++;
+        }
+        return combination;
+    }
 }
